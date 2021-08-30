b8cca4e81bd6c6a36039784d99a9fcfc950ac6ad
ShradhaNbr <sradha.nambiar@gmail.com>
2021-08-30 19:47:10 +0530

    Added : Code to save Employee Payroll Object to Local Storage

--- js/Form.js
+++ js/Form.js
@@ -1,32 +1,47 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) =>{
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-       if ( name.value.length == 0) {
-          textError.textContent = "";
-          return;
-       }
-       try {
-          (new EmployeepayrollData()).name = name.value;
-          textError.textContent = "";
-       } catch (e) {
-          textError.textContent = e;
-       }
+    name.addEventListener('input', function(){
+        if(name.value.length == 0){
+            textError.textContent ="";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent ="";
+        } catch (e){
+            textError.textContent = e;
+        }
     });
- 
+
     const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salary.addEventListener('input', function() {
-       output.textContent = salary.value;
-    });
+        const output = document.querySelector('.salary-output');
+        output.textContent = salary.value;
+        salary.addEventListener('input',function(){
+            output.textContent = salary.value;
+        });
 });
+
 const save = () => {
     try{
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch (e){
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData){
+
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
 }
 
 const createEmployeePayroll = () => {
