8a742c52a5147c61b3fc9244f689d83812f5865c
ShradhaNbr <sradha.nambiar@gmail.com>
2021-08-30 18:35:25 +0530

    Added : Code to modify with new attributes and getters and setters

---
+++ js/Form.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', (event)=> {
+    const salary = document.querySelector('#salary');
+            const output = document.querySelector('.salary-output');
+            output.textContent = salary.value;
+            salary.addEventListener('input', function () {
+                output.textContent = salary.value;
+            });
+       
+    const name=document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input',function(){
+        if(name.value.length==0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            new EmployeepayrollData().name = name.value;
+            textError.textContent = "";
+        } catch(e) {
+            textError.textContext = e;
+        }
+    });
+});
+
+
