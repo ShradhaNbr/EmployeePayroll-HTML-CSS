554d5510fa17eb796d2b4d4d7c2626bb61f50738
ShradhaNbr <sradha.nambiar@gmail.com>
2021-08-30 18:52:57 +0530

    Added : Code to validate name

--- js/EmployeepayrollData.js
+++ js/EmployeepayrollData.js
@@ -1,46 +1,70 @@
 class EmployeepayrollData {
 
-    //getter and setter method
-    get id() { return this._id; }
-    set id(id) {
-        this._id = id;
-    }
-    get name() { return this._name; }
-    set name(name) {
-        let nameRegex = RegexExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
-        if (nameRegex.test(name))
-            this.name = name;
-        else throw 'Name is Incorrect';
-    }
-    get profilePic() { return this._profilePic; }
-    set profilePic(profilePic) {
-        this._profilePic = profilePic;
-    }
-    get gender() { return this._gender }
-    set gender(gender) {
-        this._gender = gender;
-    }
-    get department() { return this._department; }
-    set department(department) {
-        this._department = department;
-    }
-    get salary() { return this._salary; }
-    set salary(salary) {
-        this._salary = salary;
-    }
-    get note() { return this._note; }
-    set note(note) {
-        this._note = note;
-    }
-    get startDate() { return this._startDate; }
-    set startDate(startDate) {
-        this._startDate = startDate;
+    get name() {
+        return this._name;
     }
 
-    //method
+    set name( name ) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+        if ( nameRegex.test(name) )
+            this._name = name;
+        else throw "Name is incorrect";
+    }
+
+    get profilePic() {
+        return this._profilePic;
+    }
+
+    set profilePic ( profilePic ) {
+        this._profilePic = profilePic;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender ( gender ) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department ( department ) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary ( salary ) {
+        this._salary = salary;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    set note ( note ) {
+        this._note = note;
+    }
+
+    get start_date() {
+        return this._start_date;
+    }
+
+    set start_date ( start_date ) {
+        this._start_date = start_date;
+    }
+
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
-        return "id=" + this.id + ", name='" + this.name + ",gender='" + this.gender + ",profilePic='" + this.profilePic + ", department=" + this.department + ", salary=" + this.salary + ",startDate=" + empDate + ",note=" + this.note;
+        const empDate = !this.start_date ? "undefined" :
+                        this.start_date.toLocaleDateString("en-US", options);
+        return 'Name = ' + this.name + ", Gender = " + this.gender + ", ProfilePic = " +this.profilePic
+                    + ", Department = " + this.department + ", Salary = " + this.salary +
+                        ", StartDate = " + empDate + ", Note = " +this.note;
     }
 }
